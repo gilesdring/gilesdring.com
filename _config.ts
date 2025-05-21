7f2a490780a35d2f379e2e035b5f3a8780e99116
Giles Dring <giles@dringtech.com>
2025-05-22 00:09:30 +0100

    Update to Lume 3

--- _config.ts
+++ _config.ts
@@ -2,30 +2,42 @@ import lume from "lume/mod.ts";
 import base_path from "lume/plugins/base_path.ts";
 import date from "lume/plugins/date.ts";
 // import inline from "lume/plugins/inline.ts";
-import liquid from "lume/plugins/liquid.ts";
+import nunjucks from "lume/plugins/nunjucks.ts";
 import metas from "lume/plugins/metas.ts";
 import minify_html from "lume/plugins/minify_html.ts";
 import sass from "lume/plugins/sass.ts";
 
 const site = lume();
 
-site.preprocess(['.njk', '.html', '.md'], (page) => {
-  if (
-    !Boolean(page.src.path.match(/^\/blog/)) ||
-    page.data.title !== undefined
-  ) return;
-  const title = page.src.path.split(/_/)[1].split(/-/).map(x => x[0].toUpperCase() + x.slice(1)).join(' ');
-  page.data.title = title;
+site.preprocess(['.md'], (pages) => {
+  for (const page of pages) {
+    if (page.data.content && page.data.content!.match(/<!--more-->/)) {
+      const excerpt = page.data.content.split('<!--more-->')[0]
+      page.data.excerpt = excerpt;
+    }
+    // const excerpt = (page.data.content && page.data.content!.match(/<!--more-->/)) ? page.data.content.split(/<!--more-->/)[0] : undefined;
+    // console.log(excerpt);
+    // if (
+    //   page.content.match(/^<!--more-->/)
+    // ) {
+    //   console.log(page.src.path);
+    // };
+  }
 });
+
+site.ignore('admin');
 
 site.use(base_path());
 site.use(date());
 // site.use(inline());
-site.use(liquid());
+site.use(nunjucks());
 site.use(metas());
 site.use(minify_html());
 site.use(sass());
 
+site.add(['.scss'])
+
 site.copy("assets/images");
+site.copy([".html"]);
 
 export default site;
