84a71840c102880686a6c8b094a649c75b35f137
Giles Dring <giles@dringtech.com>
2025-05-31 10:12:04 +0100

    Fix mimetype of webfinger

--- serve.ts
+++ serve.ts
@@ -1,24 +1,33 @@
-import Server from 'lume/core/server.ts';
+import Server, { Middleware } from 'lume/core/server.ts';
 
 const server = new Server({
   port: 8000,
   root: `${Deno.cwd()}/_site`,
 });
 
-server.use(async (request, next) => {
-  const url = new URL(request.url);
+function mimetype(typeMap: { [k: string]: string }): Middleware {
+  return async (request, next) => {
+    const url = new URL(request.url);
 
-  if (!['/fedi'].includes(url.pathname)) {
-    return next(request);
-  }
+    const type = Object.entries(typeMap).map(([match, value]) =>
+      url.pathname.match(new RegExp(match)) && value
+    ).find((t) => t != null);
 
-  // Here you can modify the request before being passed to next middlewares
-  const response = await next(request);
-  response.headers.set('content-type', 'application/activity+json;');
+    if (!type) {
+      return next(request);
+    }
 
-  // Here you can modify the response before being returned to the previous middleware
-  return response;
-});
+    const response = await next(request);
+    response.headers.set('content-type', type);
+
+    return response;
+  };
+}
+
+server.use(mimetype({
+  '/fedi': 'application/activity+json;',
+  '/.well-known/webfinger': 'application/json;',
+}));
 
 server.start();
 
