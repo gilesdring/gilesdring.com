5df86c021d8119759e199566aef99c09278c1848
Giles Dring <giles@dringtech.com>
2025-05-31 09:31:52 +0100

    Fix mime type of actor file

--- serve.ts
+++ serve.ts
@@ -5,6 +5,21 @@ const server = new Server({
   root: `${Deno.cwd()}/_site`,
 });
 
+server.use(async (request, next) => {
+  const url = new URL(request.url);
+
+  if (!['/fedi'].includes(url.pathname)) {
+    return next(request);
+  }
+
+  // Here you can modify the request before being passed to next middlewares
+  const response = await next(request);
+  response.headers.set('content-type', 'application/activity+json;');
+
+  // Here you can modify the response before being returned to the previous middleware
+  return response;
+});
+
 server.start();
 
 console.log('Listening on http://localhost:8000');
